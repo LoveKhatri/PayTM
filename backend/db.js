1f313522e2bdfa2c379d7fb92a97b8ae761a353a
ツ『Love』ツ <lovekhatri9211@gmail.com>
2024-04-23 19:16:46 +0530

    ADD: account Schema

--- backend/db.js
+++ backend/db.js
@@ -2,13 +2,50 @@ const mongoose = require('mongoose')
 
 mongoose.connect('mongodb://localhost:27017/paytm')
 
-const User = mongoose.model('user', {
-    username: String,
-    firstName: String,
-    lastName: String,
-    password: String
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true,
+        trim: true,
+        lowercase: true,
+        minLength: 3,
+        maxLength: 30
+    },
+    password: {
+        type: String,
+        required: true,
+        minLength: 6
+    },
+    firstName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+    lastName: {
+        type: String,
+        required: true,
+        trim: true,
+        maxLength: 50
+    },
+})
+
+const User = mongoose.model('user', userSchema)
+
+const Account = mongoose.model('bank', {
+    userId: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'user',
+        required: true
+    },
+    balance: {
+        type: Number,
+        required: true
+    }
 })
 
 module.exports = {
-    User
+    User,
+    Account
 }
